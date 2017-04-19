4d9d1c08efef5bdc487b46bc8fcf97cc44e0d84e
Mishka2 <michelle975@gmail.com>
2017-04-19 11:49:05 -0400

    GPS System with Location Services

--- app/src/main/java/com/example/mishka/gps/MapsActivity.java
+++ app/src/main/java/com/example/mishka/gps/MapsActivity.java
@@ -387,37 +387,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
         public void popUpMenu(View v) {
-//        LayoutInflater inflater = getLayoutInflater();
-//        View alertLayout = inflater.inflate(R.layout.layout_custom_design, null);
-//        final EditText number = (EditText) alertLayout.findViewById(R.id.et_timeDiff);
-//
-//
-//        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-//        alert.setTitle("Settings");
-//        // this is set the view from XML inside AlertDialog
-//        alert.setView(alertLayout);
-//        // disallow cancel of AlertDialog on click of back button and outside touch
-//        alert.setCancelable(false);
-//        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                Toast.makeText(getBaseContext(), "Cancel clicked", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
-//        alert.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
-//
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                Integer timeDiff = parseInt(number.getText().toString());
-//                Toast.makeText(getBaseContext(), "Time: " + timeDiff, Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//        AlertDialog dialog = alert.create();
-//        dialog.show();
-            getLocationFromAddress(getBaseContext());
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.layout_custom_design, null);
+        final EditText number = (EditText) alertLayout.findViewById(R.id.et_timeDiff);
+
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Settings");
+        // this is set the view from XML inside AlertDialog
+        alert.setView(alertLayout);
+        // disallow cancel of AlertDialog on click of back button and outside touch
+        alert.setCancelable(false);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getBaseContext(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alert.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Integer timeDiff = parseInt(number.getText().toString());
+                Toast.makeText(getBaseContext(), "Time: " + timeDiff, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        AlertDialog dialog = alert.create();
+        dialog.show();
+//            getLocationFromAddress(getBaseContext());
 //            "5 Marsh Hawk Way, West Boylston, Massachusetts"
         }
 
