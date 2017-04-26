0a9f16ba95ef02616de042f678a6a37616b8d815
Mishka2 <michelle975@gmail.com>
2017-04-26 13:07:54 -0400

    GPS System with Location Services

--- app/src/main/java/com/example/mishka/gps/EveryAdventure.java
+++ app/src/main/java/com/example/mishka/gps/EveryAdventure.java
@@ -8,26 +8,53 @@ public class EveryAdventure {
     //data
     String numContact = null;
     String addressContact = null;
-    //////////////////////////////////////////////////////////////////////////
     String currentAddress = null;
+    int ID;
+    int counter = 0;
 
     //constructor
-    public EveryAdventure(String number, String addContact){
+    public EveryAdventure(String number, String addContact, String currAddress){
         numContact = number;
         addressContact = addContact;
+        currentAddress = currAddress;
+        this.ID = counter;
+        counter++;
+    }
+
+
+    public EveryAdventure(){
+        numContact = "0";
+        addressContact = "bloopAway";
+        currentAddress = "bloop";
     }
 
     //methods
+    public int getID(){
+        return ID;
+    }
+
     public String getNumContact(){
        return numContact;
+    }
+
+    public void setNumContact(String number){
+        numContact = number;
     }
 
     public String getAddressContact(){
         return addressContact;
     }
 
+    public void setAddressContact(String address){
+        addressContact = address;
+    }
+
     public String getCurrentAddress(){
         return currentAddress;
+    }
+
+    public void setCurrentAddress(String currAdd){
+        currentAddress = currAdd;
     }
 
 }
